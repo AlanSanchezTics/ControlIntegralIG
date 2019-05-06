f630a4ba311529004c443d65e04770366cc6951c
AlanSanchezTics <alan.dsrn@gmail.com>
2019-05-06 13:08:23 -0500

    Actulizacion de bd

--- adm/database.php
+++ adm/database.php
@@ -1,5 +1,6 @@
 <?php
     date_default_timezone_set("America/Mexico_city");
-    $conn = new mysqli('localhost', 'root', '', 'u720362080_ciaig');
+    $conn = new mysqli('localhost', 'u720362080_sa', 'COOC9N7N', 'u720362080_ciig');
+    //$conn = new mysqli('localhost', 'root', '', 'u720362080_ciaig');
     $tildes = $conn->query("SET NAMES 'utf8'");
 ?>
