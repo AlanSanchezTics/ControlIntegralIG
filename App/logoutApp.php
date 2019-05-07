108809d99c6a01fb564d6bffa76ce95783746842
AlanSanchezTics <alan.dsrn@gmail.com>
2019-05-06 21:57:52 -0500

    Archivo de salida

---
+++ App/logoutApp.php
@@ -0,0 +1,13 @@
+<?php
+$json =file_get_contents("php://input");
+$obj = json_decode($json,true);
+$idAlumno = "2018"."".$obj["idAlumno"];
+
+include "database.php";
+$sql ="UPDATE tbl_usuarios SET TOKEN = NULL WHERE LOGIN = '{$idAlumno}'";
+if(mysqli_query($conexion,$sql)===TRUE){
+    echo json_encode(true);
+}else{
+    echo json_encode(false);
+}
+?>
