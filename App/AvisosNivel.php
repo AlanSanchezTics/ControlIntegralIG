85d5d3798f5a071639431ccda5d3734ed6757577
AlanSanchezTics <alan.dsrn@gmail.com>
2019-05-10 08:01:01 -0500

    Update AvisosNivel.php
    fixed AvisosNivel

--- App/AvisosNivel.php
+++ App/AvisosNivel.php
@@ -1,12 +1,9 @@
 <?php 
-	if(isset($_POST["idAlumno"])){
-		$idAlumno = $_POST["idAlumno"];
-	}else{
-		$json =file_get_contents("php://input");
-		$obj = json_decode($json,true);
-		$idAlumno = $obj["idAlumno"];
-	}
-	 include "database.php";
+	$json =file_get_contents("php://input");
+	$obj = json_decode($json,true);
+	$idAlumno = $obj["idAlumno"];
+	
+	include "database.php";
 	$sql = "SELECT ID_AVISO, TITULO_AVISO, DESCRIPCION_AVISO, IMAGEN_AVISO, FECHA_INICIAL, FECHA_FINAL FROM tbl_avisos_nivel WHERE  NIVEL= (SELECT NIVEL FROM tbl_alumnos WHERE ID_ALUMNO = {$idAlumno}) AND EXISTE = 1 AND FECHA_FINAL >= CURRENT_TIMESTAMP ORDER BY ID_AVISO DESC";
 	$result = mysqli_query($conexion,$sql);
 	
@@ -24,4 +21,4 @@
 		$datos = json_encode($arreglo);
 	}
 	echo($datos);
- ?>
+?>
