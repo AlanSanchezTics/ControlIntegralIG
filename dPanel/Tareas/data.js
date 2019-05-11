2cbac24383caafc166de57794c9cc0ae2936ae73
AlanSanchezTics <alan.dsrn@gmail.com>
2019-05-10 19:42:32 -0500

    Licencia Froala

--- dPanel/Tareas/data.js
+++ dPanel/Tareas/data.js
@@ -58,7 +58,6 @@ var limpiar_forms = function () {
     $('.imagePreview').css("background-image", "");
     $('.imagePreview').css("display",'none');
     $("i.del").css("display","none");
-    $(".fr-wrapper").find('a').remove();
 }
 var obtener_data_view = function (tbody, table) {
     $(tbody).on('click', 'button.view', function () {
