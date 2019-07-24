fe8e24b19bff48e39051365a2d36a62b12a45e58
AlanSanchezTics <alan.dsrn@gmail.com>
2019-07-24 11:39:22 -0500

    server db

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
     include_once 'error_log.php';
     set_error_handler('error');
     date_default_timezone_set("America/Mexico_city");
-    //$conn = new mysqli('localhost', 'u720362080_sa', 'COOC9N7N', 'u720362080_ciig');
-    $conn = new mysqli('localhost', 'root', '', 'u720362080_ciaig');
+    $conn = new mysqli('localhost', 'u720362080_sa', 'COOC9N7N', 'u720362080_ciig');
+    //$conn = new mysqli('localhost', 'root', '', 'u720362080_ciaig');
     $tildes = $conn->query("SET NAMES 'utf8'");
 ?>
