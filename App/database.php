c9af1d86886ad0158cc26da10af66d8f53f4efd6
AlanSanchezTics <alan.dsrn@gmail.com>
2019-07-29 11:16:41 -0500

    Utlimos Cambios

--- App/database.php
+++ App/database.php
@@ -3,8 +3,6 @@
     set_error_handler('error');
     date_default_timezone_set("America/Mexico_city");
     include '../database.php';
-    //$conexion = new mysqli('localhost', 'u720362080_sa', 'COOC9N7N', 'u720362080_ciig');
-    //$conexion = new mysqli('localhost', 'root', '', 'u720362080_ciaigserver');
     $conexion = $conn;
     $tildes = $conexion->query("SET NAMES 'utf8'");
 ?>
