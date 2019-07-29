b1d3c1070cde6c91d42f95f60ef4d553154737e2
AlanSanchezTics <alan.dsrn@gmail.com>
2019-07-29 13:09:46 -0500

    Ultimos Cambios

--- App/logoutApp.php
+++ App/logoutApp.php
@@ -3,10 +3,10 @@ include_once '../error_log.php';
 set_error_handler('error');
 $json =file_get_contents("php://input");
 $obj = json_decode($json,true);
-$idAlumno = "2018"."".$obj["idAlumno"];
+$idUsuario = $obj["idUsuario"];
 
 include "database.php";
-$sql ="UPDATE tbl_usuarios SET TOKEN = NULL WHERE LOGIN = '{$idAlumno}'";
+$sql ="UPDATE tbl_usuarios SET TOKEN = NULL WHERE ID_USUARIO = {$idUsuario}";
 if(mysqli_query($conexion,$sql)===TRUE){
     echo json_encode(true);
 }else{
