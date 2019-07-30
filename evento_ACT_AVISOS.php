01e835f2e059cac0f7b0e6c9cf697a4788f93975
AlanSanchezTics <alan.dsrn@gmail.com>
2019-07-30 11:27:12 -0500

    Cron Jobs

--- evento_ACT_AVISOS.php
+++ evento_ACT_AVISOS.php
@@ -2,11 +2,13 @@
     include 'database.php';
 
     $sql = "CREATE EVENT `ACT_AG` ON SCHEDULE AT NOW() DO UPDATE tbl_avisosgenerales SET PROGRAMADO = 0 WHERE FECHA_INICIAL <= CURRENT_TIMESTAMP";
-    mysqli_query($sql);
+    mysqli_query($conn,$sql);
     $sql = "CREATE EVENT `ACT_AN` ON SCHEDULE AT NOW() DO UPDATE tbl_avisos_nivel SET PROGRAMADO = 0 WHERE FECHA_INICIAL <= CURRENT_TIMESTAMP";
-    mysqli_query($sql);
+    mysqli_query($conn,$sql);
     $sql = "CREATE EVENT `ACT_AGP` ON SCHEDULE AT NOW() DO UPDATE tbl_avisos_grupo SET PROGRAMADO = 0 WHERE FECHA_INICIAL <= CURRENT_TIMESTAMP";
-    mysqli_query($sql);
+    mysqli_query($conn,$sql);
     $sql = "CREATE EVENT `ACT_AP` ON SCHEDULE AT NOW() DO UPDATE tbl_avisos_alumno SET PROGRAMADO = 0 WHERE FECHA_INICIAL <= CURRENT_TIMESTAMP";
-    mysqli_query($sql);
+    mysqli_query($conn,$sql);
+
+    echo "Avisos Actualizados";
 ?>
--- evento_ACT_TAREAS.php
+++ evento_ACT_TAREAS.php
@@ -2,5 +2,7 @@
     include 'database.php';
 
     $sql = "CREATE EVENT `ACT_TAREAS` ON SCHEDULE AT NOW() DO UPDATE tbl_tareas SET PROGRAMADO = 0 WHERE FECHA_CREACION <= CURRENT_TIMESTAMP";
-    mysqli_query($sql);
+    mysqli_query($conn,$sql);
+
+    echo "Tareas actualizadas";
 ?>
